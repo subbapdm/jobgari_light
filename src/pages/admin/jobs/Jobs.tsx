a0b09b345606104543402e6c2a6ae27ec779e8bb
Padam <subbapdm@gmail.com>
2026-05-08 23:45:33 +0200

    added checkbox and drodown on table

--- src/pages/admin/jobs/Jobs.tsx
+++ src/pages/admin/jobs/Jobs.tsx
@@ -1,10 +1,12 @@
 import FormSelect from "@/components/form/FormSelect";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { jobsService } from "@/services/jobService";
 import { useQuery } from "@tanstack/react-query";
-import { Download, Plus, Search } from "lucide-react";
+import { Download, Eye, MoreHorizontalIcon, Plus, Search, Trash } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -54,12 +56,12 @@ const Jobs = () => {
       <div className="space-y-4">
          <div className="flex items-center justify-between">
             <div>
-               <h2 className="text-2xl font-bold text-slate-800">Jobs</h2>
-               <p className="text-sm text-slate-400">{""} total jobs</p>
+               <h2 className="text-2xl font-bold text-slate-800">Manage jobs</h2>
+               <p className="text-sm text-slate-400">Manage your job postings and track applications.</p>
             </div>
-            <Button onClick={() => navigate("/admin/jobs/create")} className="bg-teal-600 hover:bg-teal-700 min-h-11">
-               <Plus className="size-4" />
-               Post Job
+            <Button onClick={() => navigate("/admin/jobs/create")} className="bg-teal-600 hover:bg-teal-600/90 min-h-11 px-4">
+               <Plus className="size-5" />
+               Post job
             </Button>
          </div>
 
@@ -71,7 +73,7 @@ const Jobs = () => {
                   options={TYPE_OPTIONS}
                   label="Types"
                   placeholder="All Types"
-                  className="w-full min-h-11 rounded-sm"
+                  className="w-full min-h-11 rounded-sm bg-white"
                />
             </div>
             <div className="flex-1">
@@ -81,7 +83,7 @@ const Jobs = () => {
                   options={STATUS_OPTIONS}
                   label="Status"
                   placeholder="All Statuses"
-                  className="w-full min-h-11 rounded-sm"
+                  className="w-full min-h-11 rounded-sm bg-white"
                />
             </div>
             <div className="relative flex-2">
@@ -90,7 +92,7 @@ const Jobs = () => {
                   placeholder="Search Jobs..."
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
-                  className="w-full pl-9 min-h-11 rounded-sm"
+                  className="w-full pl-9 min-h-11 rounded-sm bg-white"
                />
             </div>
             <div>
@@ -100,18 +102,17 @@ const Jobs = () => {
                </Button>
             </div>
          </div>
-
-
          
          <div className="bg-white overflow-x-auto rounded-lg relative">
          
             <table className="w-full mx-auto">
-               <thead className="border-b border-dashed border-gray-200 text-gray-400 text-left text-[0.85rem]">
+               <thead className="border-b border-dashed border-gray-200 text-gray-500 text-left text-[0.85rem]">
                   <tr>
                      <th className="p-4 font-medium">Job</th>
                      <th className="p-4 font-medium">Company</th>
                      <th className="p-4 font-medium">Type</th>
                      <th className="p-4 font-medium">Status</th>
+                     <th className="p-4 font-medium">Applications</th>
                      <th className="p-4 font-medium">Deadline</th>
                      <th className="p-4 font-medium">Actions</th>
                   </tr>
@@ -120,6 +121,13 @@ const Jobs = () => {
 
                  {data?.jobs.map((job) => (
                      <tr key={job._id} className="hover:bg-gray-50 transition-colors">
+                        <td className="p-4">
+                           <Checkbox
+                              checked={false}
+                              onCheckedChange={() => {}}
+                              className="h-5 w-5 data-[state=checked]:bg-blue-500 data-[state=checked]:border-blue-500"
+                           />
+                        </td>
                         <td className="px-4 py-3 font-medium text-gray-800">
                            <div className="flex items-center gap-3">
                               <div className="size-10 bg-slate-300 flex items-center justify-center text-lg font-bold text-slate-400 rounded-full p-0.5 shrink-0">
@@ -130,29 +138,48 @@ const Jobs = () => {
                                  )}
                               </div>
                               <div className="flex flex-col">
-                                 <strong className="leading-tight">{job.title}</strong>
+                                 <strong className="leading-tight text-[1rem]">{job.title}</strong>
                                  <span className="text-xs text-gray-400">{job.category.name}</span>
                               </div>
                            </div>
                         </td>
                         <td className="px-4 py-3 text-gray-500">
-                           <strong className="text-[0.9rem] text-slate-600">{job.company.name}</strong>
+                           <strong className="text-[0.9rem] font-semibold text-slate-600">{job.company.name}</strong>
                         </td>
                         <td className="px-4 py-3 text-[0.8rem] font-medium text-gray-500 capitalize">{job.jobType}</td>
                         <td className="px-4 py-3">
                            {job.status === "active" ? (
                               <Badge className="min-h-6 px-2.5 bg-teal-100 text-teal-600">{job.status}</Badge>
                            ) : (
-
                               <Badge className="min-h-6 px-2.5 bg-slate-100 text-slate-500">{job.status}</Badge>
                            )}
-                          
+                        </td>
+                        <td className="px-4 py-3 text-gray-500">
+                           <Badge className="min-h-6 bg-slate-100 text-slate-500 font-semibold">443</Badge>
                         </td>
                         <td className="px-4 py-3 text-sm text-gray-500">
                            {new Date(Date.now()).toLocaleDateString()}
                         </td>
                         <td className="px-4 py-3 font-medium text-gray-800">
-                        
+                           <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                 <Button variant="ghost" size="icon" className="size-8 cursor-pointer">
+                                    <MoreHorizontalIcon />
+                                    <span className="sr-only">Open menu</span>
+                                 </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent align="end">
+                                 <DropdownMenuItem onClick={() => {}}>
+                                    <Eye size={13} />
+                                    View Profile
+                                 </DropdownMenuItem>
+                                 <DropdownMenuSeparator />
+                                 <DropdownMenuItem variant="destructive" onClick={() => {}}>
+                                    <Trash size={13} />
+                                    Delete
+                                 </DropdownMenuItem>
+                              </DropdownMenuContent>
+                           </DropdownMenu>
                         </td>
                      </tr>
                  ))}
